fb3b241dbfffab7f5b7e1da4695176d8969a7b71
Basyrov Rustam <hrustbas@gmail.com>
2025-02-10 09:10:42 +0300

    pretty comments

--- rb_alloc.h
+++ rb_alloc.h
@@ -1,9 +1,9 @@
 /*  Region struct: 
-      *start -- pointer to begin of region 
-      cursor -- pointer to begin of allocated memory 
-      size   -- size of region (in bytes) 
-      count  -- count of allocated objects 
-*/
+ *    *start -- pointer to begin of region 
+ *    cursor -- pointer to begin of allocated memory 
+ *    size   -- size of region (in bytes) 
+ *    count  -- count of allocated objects 
+ */
 typedef struct region {
   char *start;
   size_t cursor;
@@ -18,13 +18,15 @@ void regionFree(Region *region);
 
 #ifdef RB_ALLOC_IMPLEMENTATION
 /*  Default region size (in bytes) 
-    You may change this value to avoid region reallocation  
-*/
+ *  You may change this value to avoid region reallocation  
+ */
 #ifndef REGION_SIZE
 #define REGION_SIZE 2048
 #endif
 
-/* For development purposes */
+/* 
+ * For development purposes 
+ */
 #define UNIMPLEMENTED                                                          \
   do {                                                                         \
     fprintf(stderr, "%s() is not implemented yet\n", __func__);                \
@@ -32,12 +34,13 @@ void regionFree(Region *region);
   } while (0)
 
 
-/*  Initialization function 
-      IN: 
-          size -- memory size to allocation (in bytes) *
-      OUT: 
-          Region* -- pointer to Region object 
-      NOTE: All memory initializated by 0 
+/*  
+ *  Initialization function 
+ *    IN: 
+ *        size -- memory size to allocation (in bytes) *
+ *    OUT: 
+ *        Region* -- pointer to Region object 
+ *  NOTE: All memory initializated by 0 
 */ 
 Region *regionInit(size_t size) {
   Region *region = malloc(sizeof(Region));
@@ -52,15 +55,15 @@ Region *regionInit(size_t size) {
   return region;
 }
 
-/* This function increase region size */
-/* IN: */
-/*     *region -- pointer to Region object */
-/*     size    -- needed size to alloc  */
-/* OUT: */
-/*     *Region -- pointer to reallocated Region */
-/* */
-/* NOTE: It is not needed to increase region size by yourself */
-/*       This function called by `regionAlloc` */
+/*
+ *  This function increase region size
+ *    IN:
+ *      size    -- needed size to alloc 
+ *    OUT:
+ *      *Region -- pointer to reallocated Region
+ *  NOTE: It is not needed to increase region size by yourself
+ *        This function called by `regionAlloc`
+ */
 void *regionIncrease(Region *region, size_t size) {
   size_t n = 2;
   while (region->size + size >= region->size * n)
@@ -74,13 +77,14 @@ void *regionIncrease(Region *region, size_t size) {
   return (void *)(region->start + region->cursor - size);
 }
 
-/* Allocation function */
-/* IN: */
-/*     *region -- pointer to Region object */
-/*     size    -- memory size to allocation (in bytes) */
-/* OUT: */
-/*     (void*) -- pointer to allocated memory */
-/* */
+/*
+ *  Allocation function
+ *    IN:
+ *      *region -- pointer to Region object 
+ *      size    -- memory size to allocation (in bytes)
+ *    OUT:
+ *      (void*) -- pointer to allocated memory
+ */
 void *regionAlloc(Region *region, size_t size) {
   if (region->cursor + size >= region->size) {
     return regionIncrease(region, size);
@@ -91,9 +95,11 @@ void *regionAlloc(Region *region, size_t size) {
   return (void *)(region->start + region->cursor - size);
 }
 
-/* This free allocated memory */
-/* IN: */
-/*     *region -- pointer to Region object */
+/*
+ *  This function free allocated memory
+ *  IN:
+ *    *region -- pointer to Region object 
+ */
 void regionFree(Region *region) {
   free(region->start);
   region->start = NULL;
@@ -102,10 +108,12 @@ void regionFree(Region *region) {
   region->count = 0;
 }
 
-/* Dumping function */
-/* Prints info about Region object */
-/* IN: */
-/*     *region -- Region object */
+/*
+ *  Dumping function
+ *  Prints info about Region object
+ *  IN:
+ *    *region -- Region object 
+ */
 void regionDump(Region *region) {
   printf("  Region dump:\n");
   printf("    cursor: %u\n", (unsigned) region->cursor);
